6f482752f04080dc7644f6d7f911b886397e1243
root <+>
2023-10-16 22:38:18 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+function submitData(name, email) {
+    const user = {
+        name: name,
+        email: email
+    }
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
